d19517c68695479e8fc3229d19c23bfe41ad2be8
daniel-azu <danielaz@wix.com>
2024-03-31 15:08:53 +0300

    wip

--- src/date-formatter.ts
+++ src/date-formatter.ts
@@ -1,17 +1,5 @@
 // ./src/date-formatter.ts
 
-/* List of all available parsing tokens
------------------------------------------
-Input   Example             Description
------------------------------------------
-YY	    01	                Two-digit year
-YYYY	  2001	              Four-digit year
-MM	    01-12	              Month, 2-digits
-MMM	    Jan-Dec	            The abbreviated month name
-D	      1-31	              Day of month
-DD	    01-31	              Day of month, 2-digits
-*/
-
 export function formatDate (format: string, date: Date): string {
   return 'Apr-2-2026'
 
